828a8a7f3607b8ca8678fc7b83ae6fa86ecf07c4
peepal67 <prayashthapaliya47@gmail.com>
2026-08-13 21:50:35 +0545

    cpp and h files switch

--- src/Wall.h
+++ src/Wall.h
@@ -1,29 +1,17 @@
-#include "Wall.h"
+#pragma once
+#include "raylib.h"
 
-Wall::Wall(Vector3 pos, Vector3 sz, Color col, Texture2D* texture)
-    : position(pos), size(sz)
-{
-    Mesh m = GenMeshCube(size.x, size.y, size.z);
-    model = LoadModelFromMesh(m);
+class Wall {
+public:
+    Wall(Vector3 pos, Vector3 size, Color color, Texture2D* texture = nullptr);
+    ~Wall();
 
-    if (texture != nullptr) {
-        model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = *texture;
-    } else {
-        model.materials[0].maps[MATERIAL_MAP_DIFFUSE].color = col;
-    }
-}
+    void Draw() const;
+    BoundingBox GetBoundingBox() const;
 
-Wall::~Wall() {
-    UnloadModel(model);
-}
+    Vector3 position;
+    Vector3 size;
 
-void Wall::Draw() const {
-    DrawModel(model, position, 1.0f, WHITE);
-}
-
-BoundingBox Wall::GetBoundingBox() const {
-    return {
-        { position.x - size.x/2, position.y - size.y/2, position.z - size.z/2 },
-        { position.x + size.x/2, position.y + size.y/2, position.z + size.z/2 }
-    };
-}
+private:
+    Model model;
+};
